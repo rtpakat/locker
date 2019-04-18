079d8eb2a3441b84f5955253f576e50c721eb3ea
Rungtip Proneiam <rungtip.rtpa@gmail.com>
2019-04-18 12:58:41 +0700

    initial commit

--- src/server/index.js
+++ src/server/index.js
@@ -207,16 +207,16 @@ mongoose
     console.log(err);
   });
 
-app.use(favicon(__dirname + "/build/favicon.ico"));
-// the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, "build")));
-app.get("/ping", function(req, res) {
-  return res.send("pong");
-});
-app.get("/*", function(req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
+// app.use(favicon(__dirname + "/build/favicon.ico"));
+// // the __dirname is the current directory from where the script is running
+// app.use(express.static(__dirname));
+// app.use(express.static(path.join(__dirname, "build")));
+// app.get("/ping", function(req, res) {
+//   return res.send("pong");
+// });
+// app.get("/*", function(req, res) {
+//   res.sendFile(path.join(__dirname, "build", "index.html"));
+// });
 
 const port = process.env.PORT || 4000;
 app.listen(port);
